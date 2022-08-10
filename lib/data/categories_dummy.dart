5405ab1da3918510230e4e401c1c10912a40cfc7
Reza Ageng <rezaageng5@gmail.com>
2022-08-10 11:14:32 +0700

    feat(models): :sparkles: add category model and data

---
+++ lib/data/categories_dummy.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../models/category_model.dart';
+
+const List<CategoryModel> dummyCategories = [
+  CategoryModel(
+    id: 'c1',
+    name: 'Mondstadt',
+    color: Colors.teal,
+  ),
+  CategoryModel(
+    id: 'c2',
+    name: 'Liyue',
+    color: Colors.amber,
+  ),
+  CategoryModel(
+    id: 'c3',
+    name: 'Inazuma',
+    color: Colors.purple,
+  ),
+  CategoryModel(
+    id: 'c4',
+    name: 'Potions',
+    color: Colors.lightGreen,
+  ),
+  CategoryModel(
+    id: 'c5',
+    name: 'ATK Boosting',
+    color: Colors.red,
+  ),
+  CategoryModel(
+    id: 'c6',
+    name: 'DEF Boosting',
+    color: Colors.blue,
+  ),
+  CategoryModel(
+    id: 'c7',
+    name: 'Recovery Dishes',
+    color: Colors.green,
+  ),
+  CategoryModel(
+    id: 'c8',
+    name: 'Adventurer\'s Dishes',
+    color: Colors.grey,
+  ),
+];
