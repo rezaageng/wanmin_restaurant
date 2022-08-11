dbdcb1337ad5ada0797702a02177909ba8981b03
Reza Ageng <rezaageng5@gmail.com>
2022-08-11 13:35:13 +0700

    feat(main): :truck: rename app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Wanmin Restaurant',
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.system,
