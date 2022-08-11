d1ac3a2522f3c902296125f77172bfab3b1d44ad
Reza Ageng <rezaageng5@gmail.com>
2022-08-11 17:58:12 +0700

    feat(food): :sparkles: add food detail screen

--- lib/screens/food_detail.dart
+++ lib/screens/food_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/food_dummy.dart';
+
 class FoodDetail extends StatelessWidget {
   static const routeName = '/food-detail';
 
@@ -8,13 +10,56 @@ class FoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context)!.settings.arguments as String;
+    final dish = foodDummy.firstWhere((food) => food.id == routeArgs);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Food Detail $routeArgs'),
+        title: Text(dish.title),
       ),
-      body: const Center(
-        child: Text('Paimon is an emergency food'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Image.network(
+              dish.imgUrl,
+              width: double.infinity,
+              height: 226,
+              fit: BoxFit.contain,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Column(
+                children: [
+                  Text(
+                    dish.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Text(
+                    dish.effect,
+                    style: Theme.of(context).textTheme.subtitle1,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Ingredients',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  ...dish.ingredients
+                      .map((ingredient) => Text('• $ingredient'))
+                      .toList(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
