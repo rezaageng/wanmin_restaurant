c1b356da3448370a8e1686313f47767f7775e1cd
Reza Ageng <rezaageng5@gmail.com>
2022-08-12 10:25:39 +0700

    style(theme): :lipstick: update dark theme

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -4,13 +4,14 @@ import 'package:flutter/services.dart';
 ThemeData darkTheme = ThemeData(
   colorScheme: const ColorScheme.dark(),
   appBarTheme: const AppBarTheme(
-    color: Colors.black,
+    color: Color.fromRGBO(18, 18, 18, 1),
     elevation: 0,
     systemOverlayStyle: SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.light,
     ),
   ),
-  canvasColor: Colors.black,
+  canvasColor: const Color.fromRGBO(18, 18, 18, 1),
+  cardTheme: const CardTheme(color: Color.fromRGBO(24, 24, 24, 1)),
   fontFamily: 'NotoSansJP',
 );
