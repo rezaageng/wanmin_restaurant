87ca8c08794651e40bc09aa921c9c39bb38afe33
Reza Ageng <rezaageng5@gmail.com>
2022-08-13 10:45:50 +0700

    feat(filter): :sparkles: add switch tile

--- lib/screens/filter.dart
+++ lib/screens/filter.dart
@@ -2,28 +2,52 @@ import 'package:flutter/material.dart';
 
 import '../navigations/left_drawer.dart';
 
-class Filter extends StatelessWidget {
+class Filter extends StatefulWidget {
   static const routeName = '/filter';
 
   const Filter({Key? key}) : super(key: key);
 
   @override
+  State<Filter> createState() => _FilterState();
+}
+
+class _FilterState extends State<Filter> {
+  bool _special = false;
+  bool _exclusive = false;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Filter'),
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () => Scaffold.of(context).openDrawer(),
-            enableFeedback: false,
+        appBar: AppBar(
+          title: const Text('Filter'),
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              enableFeedback: false,
+            ),
           ),
         ),
-      ),
-      drawer: const LeftDrawer(),
-      body: const Center(
-        child: Text('You are my favorite'),
-      ),
-    );
+        drawer: const LeftDrawer(),
+        body: ListView(
+          children: [
+            SwitchListTile.adaptive(
+              enableFeedback: false,
+              value: _special,
+              onChanged: (value) => setState(() {
+                _special = value;
+              }),
+              title: const Text('Special Dish'),
+            ),
+            SwitchListTile.adaptive(
+              enableFeedback: false,
+              value: _exclusive,
+              onChanged: (value) => setState(() {
+                _exclusive = value;
+              }),
+              title: const Text('Exclusive Dish'),
+            ),
+          ],
+        ));
   }
 }
